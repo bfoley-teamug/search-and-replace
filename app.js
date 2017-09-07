e3d900384512a4cfc4fba4be80bf1e21b008a483
bfoley-teamug <withtechron@gmail.com>
2017-09-07 15:19:49 +0000

    tweak

--- app.js
+++ app.js
@@ -10,14 +10,29 @@ const before = document.getElementById('before');
 const after = document.getElementById('after'); 
 
 button.addEventListener('click', (e) => {
+   
+  
+  
    function replaceWord(text, before, after) {
     const index = text.indexOf(before);
+      if (input.value == 0 || inputB.value == 0) { 
+        alert("Please fill 'em both out"); 
+        return ' '; 
+      }   
+     //this part doesn't work ... 
+//        else if (input.value !== before.indexOf()) {
+//          alert("That word ain't there");
+//          return ' ';
+//        }
+     
+       else 
          if (text[index] === text[index].toUpperCase()) {
           after = after.charAt(0).toUpperCase() + after.slice(1);
         }
-     text = text.replace(new RegExp(before, 'g'), after); //global replace 
+     text = text.replace(new RegExp(before, 'g'), after); //global replace       
      return text;
-      }
+      
+    }
   
    var output = replaceWord(before.textContent, input.value, inputB.value);
    console.log(output); 
